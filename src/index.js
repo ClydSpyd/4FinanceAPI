ad10db6651cfafac8c238ff7666749ac55c26292
Dmitry Shenk <dmitry.shenk@4finance.lt>
2016-10-27 19:21:27 +0300

    Use PORT system variable as default

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import lowdb from 'lowdb';
 import loansApiServer from './server.js';
 
-const port = process.env.LOANS_API_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 loansApiServer(port, lowdb());
